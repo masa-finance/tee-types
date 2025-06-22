d1bd5096ee394b37b84f5ad865ad7ba03313ea96
Alvin Reyes <me@areyes.onl>
2025-06-22 15:28:10 -0400

    add linkedin

--- args/linkedin.go
+++ args/linkedin.go
@@ -4,7 +4,7 @@ package args
 type LinkedInSearchArguments struct {
 	QueryType      string   `json:"type"`                       // "searchbyquery", "getprofile"
 	Query          string   `json:"query"`                      // Keywords for search or username for profile
-	NetworkFilters []string `json:"network_filters,omitempty"` // ["F", "S", "O"] - First, Second, Other
+	NetworkFilters []string `json:"network_filters,omitempty"` // ["F", "S", "O"] - First, Second, Other (default: all)
 	MaxResults     int      `json:"max_results"`                // Maximum number of results to return
 	Start          int      `json:"start"`                      // Pagination start offset
 }
