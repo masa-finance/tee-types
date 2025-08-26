0f8ead970fa4ee1d1b09ad991417473c056aa9f9
grantdfoster <grantdfoster@gmail.com>
2025-08-26 19:01:22 +0200

    fix: tiktok json labeling for search by query types

--- types/tiktok.go
+++ types/tiktok.go
@@ -14,49 +14,49 @@ type TikTokSearchByQueryResult struct {
 	URL                   string            `json:"url"`
 	ID                    string            `json:"id"`
 	Desc                  string            `json:"desc"`
-	CreateTime            string            `json:"create_time"`
-	ScheduleTime          int64             `json:"schedule_time"`
+	CreateTime            string            `json:"createTime"`
+	ScheduleTime          int64             `json:"scheduleTime"`
 	Video                 TikTokVideo       `json:"video"`
 	Author                string            `json:"author"`
 	Music                 TikTokMusic       `json:"music"`
 	Challenges            []any             `json:"challenges"` // we don't have examples of this data yet...
 	Stats                 TikTokStats       `json:"stats"`
-	IsActivityItem        bool              `json:"is_activity_item"`
-	DuetInfo              TikTokDuetInfo    `json:"duet_info"`
-	WarnInfo              []any             `json:"warn_info"` // we don't have examples of this data yet...
-	OriginalItem          bool              `json:"original_item"`
-	OfficalItem           bool              `json:"offical_item"`
-	TextExtra             []TikTokTextExtra `json:"text_extra"`
+	IsActivityItem        bool              `json:"isActivityItem"`
+	DuetInfo              TikTokDuetInfo    `json:"duetInfo"`
+	WarnInfo              []any             `json:"warnInfo"` // we don't have examples of this data yet...
+	OriginalItem          bool              `json:"originalItem"`
+	OfficialItem          bool              `json:"officalItem"`
+	TextExtra             []TikTokTextExtra `json:"textExtra"`
 	Secret                bool              `json:"secret"`
-	ForFriend             bool              `json:"for_friend"`
+	ForFriend             bool              `json:"forFriend"`
 	Digged                bool              `json:"digged"`
-	ItemCommentStatus     int               `json:"item_comment_status"`
-	ShowNotPass           bool              `json:"show_not_pass"`
+	ItemCommentStatus     int               `json:"itemCommentStatus"`
+	ShowNotPass           bool              `json:"showNotPass"`
 	VL1                   bool              `json:"vl1"`
-	TakeDown              int               `json:"take_down"`
-	ItemMute              bool              `json:"item_mute"`
-	EffectStickers        []any             `json:"effect_stickers"` // we don't have examples of this data yet...
-	AuthorStats           TikTokAuthorStats `json:"author_stats"`
-	PrivateItem           bool              `json:"private_item"`
-	DuetEnabled           bool              `json:"duet_enabled"`
-	StitchEnabled         bool              `json:"stitch_enabled"`
-	StickersOnItem        []any             `json:"stickers_on_item"` // we don't have examples of this data yet...
-	IsAd                  bool              `json:"is_ad"`
-	ShareEnabled          bool              `json:"share_enabled"`
+	TakeDown              int               `json:"takeDown"`
+	ItemMute              bool              `json:"itemMute"`
+	EffectStickers        []any             `json:"effectStickers"` // we don't have examples of this data yet...
+	AuthorStats           TikTokAuthorStats `json:"authorStats"`
+	PrivateItem           bool              `json:"privateItem"`
+	DuetEnabled           bool              `json:"duetEnabled"`
+	StitchEnabled         bool              `json:"stitchEnabled"`
+	StickersOnItem        []any             `json:"stickersOnItem"` // we don't have examples of this data yet...
+	IsAd                  bool              `json:"isAd"`
+	ShareEnabled          bool              `json:"shareEnabled"`
 	Comments              []any             `json:"comments"` // we don't have examples of this data yet...
-	DuetDisplay           int               `json:"duet_display"`
-	StitchDisplay         int               `json:"stitch_display"`
-	IndexEnabled          bool              `json:"index_enabled"`
-	DiversificationLabels []string          `json:"diversification_labels"`
-	AdAuthorization       bool              `json:"ad_authorization"`
-	AdLabelVersion        int               `json:"ad_label_version"`
-	LocationCreated       string            `json:"location_created"`
+	DuetDisplay           int               `json:"duetDisplay"`
+	StitchDisplay         int               `json:"stitchDisplay"`
+	IndexEnabled          bool              `json:"indexEnabled"`
+	DiversificationLabels []string          `json:"diversificationLabels"`
+	AdAuthorization       bool              `json:"adAuthorization"`
+	AdLabelVersion        int               `json:"adLabelVersion"`
+	LocationCreated       string            `json:"locationCreated"`
 	Nickname              string            `json:"nickname"`
-	AuthorID              string            `json:"author_id"`
-	AuthorSecID           string            `json:"author_sec_id"`
-	AvatarThumb           string            `json:"avatar_thumb"`
-	DownloadSetting       int               `json:"download_setting"`
-	AuthorPrivate         bool              `json:"author_private"`
+	AuthorID              string            `json:"authorId"`
+	AuthorSecID           string            `json:"authorSecId"`
+	AvatarThumb           string            `json:"avatarThumb"`
+	DownloadSetting       int               `json:"downloadSetting"`
+	AuthorPrivate         bool              `json:"authorPrivate"`
 }
 
 type TikTokSearchByTrending struct {
@@ -82,18 +82,18 @@ type TikTokVideo struct {
 	PlayAddr      string              `json:"play_addr"`
 	DownloadAddr  string              `json:"download_addr"`
 	ShareCover    []string            `json:"share_cover"`
-	ReflowCover   string              `json:"reflow_cover"`
+	ReflowCover   string              `json:"reflowCover"`
 	Bitrate       int                 `json:"bitrate"`
-	EncodedType   string              `json:"encoded_type"`
+	EncodedType   string              `json:"encodedType"`
 	Format        string              `json:"format"`
-	VideoQuality  string              `json:"video_quality"`
-	EncodeUserTag string              `json:"encode_user_tag"`
-	CodecType     string              `json:"codec_type"`
+	VideoQuality  string              `json:"videoQuality"`
+	EncodeUserTag string              `json:"encodeUserTag"`
+	CodecType     string              `json:"codecType"`
 	Definition    string              `json:"definition"`
-	SubtitleInfos []any               `json:"subtitle_infos"` // we don't have examples of this data yet...
-	ZoomCover     TikTokZoomCover     `json:"zoom_cover"`
-	VolumeInfo    TikTokVolumeInfo    `json:"volume_info"`
-	BitrateInfo   []TikTokBitrateInfo `json:"bitrate_info"`
+	SubtitleInfos []any               `json:"subtitleInfos"` // we don't have examples of this data yet...
+	ZoomCover     TikTokZoomCover     `json:"zoomCover"`
+	VolumeInfo    TikTokVolumeInfo    `json:"volumeInfo"`
+	BitrateInfo   []TikTokBitrateInfo `json:"bitrateInfo"`
 }
 
 type TikTokZoomCover struct {
@@ -104,71 +104,71 @@ type TikTokZoomCover struct {
 }
 
 type TikTokVolumeInfo struct {
-	Loudness float64 `json:"loudness"`
-	Peak     float64 `json:"peak"`
+	Loudness float64 `json:"Loudness"`
+	Peak     float64 `json:"Peak"`
 }
 
 type TikTokBitrateInfo struct {
-	GearName    string         `json:"gear_name"`
+	GearName    string         `json:"GearName"`
 	Bitrate     int            `json:"bitrate"`
-	QualityType int            `json:"quality_type"`
-	PlayAddr    TikTokPlayAddr `json:"play_addr"`
-	CodecType   string         `json:"codec_type"`
+	QualityType int            `json:"QualityType"`
+	PlayAddr    TikTokPlayAddr `json:"PlayAddr"`
+	CodecType   string         `json:"CodecType"`
 }
 
 type TikTokPlayAddr struct {
-	Uri      string   `json:"uri"`
-	UrlList  []string `json:"url_list"`
-	DataSize string   `json:"data_size"`
-	UrlKey   string   `json:"url_key"`
-	FileHash string   `json:"file_hash"`
-	FileCs   string   `json:"file_cs"`
+	Uri      string   `json:"Uri"`
+	UrlList  []string `json:"UrlList"`
+	DataSize string   `json:"DataSize"`
+	UrlKey   string   `json:"UrlKey"`
+	FileHash string   `json:"FileHash"`
+	FileCs   string   `json:"FileCs"`
 }
 
 type TikTokMusic struct {
 	ID                 string `json:"id"`
 	Title              string `json:"title"`
-	PlayURL            string `json:"play_url"`
-	CoverLarge         string `json:"cover_large"`
-	CoverMedium        string `json:"cover_medium"`
-	CoverThumb         string `json:"cover_thumb"`
-	AuthorName         string `json:"author_name"`
+	PlayURL            string `json:"playUrl"`
+	CoverLarge         string `json:"coverLarge"`
+	CoverMedium        string `json:"coverMedium"`
+	CoverThumb         string `json:"coverThumb"`
+	AuthorName         string `json:"authorName"`
 	Original           bool   `json:"original"`
 	Duration           int    `json:"duration"`
 	Album              string `json:"album"`
-	ScheduleSearchTime int64  `json:"schedule_search_time"`
+	ScheduleSearchTime int64  `json:"scheduleSearchTime"`
 }
 
 type TikTokStats struct {
-	DiggCount    int64 `json:"digg_count"`
-	ShareCount   int64 `json:"share_count"`
-	CommentCount int64 `json:"comment_count"`
-	PlayCount    int64 `json:"play_count"`
+	DiggCount    int64 `json:"diggCount"`
+	ShareCount   int64 `json:"shareCount"`
+	CommentCount int64 `json:"commentCount"`
+	PlayCount    int64 `json:"playCount"`
 }
 
 type TikTokDuetInfo struct {
-	DuetFromID string `json:"duet_from_id"`
+	DuetFromID string `json:"duetFromId"`
 }
 
 type TikTokTextExtra struct {
-	AwemeID      string `json:"aweme_id"`
+	AwemeID      string `json:"awemeId"`
 	Start        int    `json:"start"`
 	End          int    `json:"end"`
-	HashtagID    string `json:"hashtag_id"`
-	HashtagName  string `json:"hashtag_name"`
+	HashtagID    string `json:"hashtagId"`
+	HashtagName  string `json:"hashtagName"`
 	Type         int    `json:"type"`
-	SubType      int    `json:"sub_type"`
-	UserID       string `json:"user_id"`
-	IsCommerce   bool   `json:"is_commerce"`
-	UserUniqueID string `json:"user_unique_id"`
-	SecUID       string `json:"sec_uid"`
+	SubType      int    `json:"subType"`
+	UserID       string `json:"userId"`
+	IsCommerce   bool   `json:"isCommerce"`
+	UserUniqueID string `json:"userUniqueId"`
+	SecUID       string `json:"secUid"`
 }
 
 type TikTokAuthorStats struct {
-	FollowerCount  int64 `json:"follower_count"`
-	FollowingCount int64 `json:"following_count"`
+	FollowerCount  int64 `json:"followerCount"`
+	FollowingCount int64 `json:"followingCount"`
 	Heart          int64 `json:"heart"`
-	HeartCount     int64 `json:"heart_count"`
-	VideoCount     int64 `json:"video_count"`
-	DiggCount      int64 `json:"digg_count"`
+	HeartCount     int64 `json:"heartCount"`
+	VideoCount     int64 `json:"videoCount"`
+	DiggCount      int64 `json:"diggCount"`
 }
